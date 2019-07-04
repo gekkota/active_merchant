36b97fce885a2f34dc0d9388550ee749787ab086
Ola <olI@gekkota.co.uk>
2019-07-04 18:02:51 +0200

    update version

--- lib/active_merchant/billing/gateways/realex.rb
+++ lib/active_merchant/billing/gateways/realex.rb
@@ -236,7 +236,7 @@ module ActiveMerchant
 
 
           xml.tag! 'pares', options[:pares] unless options[:pares].blank?
-          puts shamaker(timestamp,money)
+          puts shamaker(timestamp,money, options)
           xml.tag! 'sha1hash', sha1from("#{timestamp}.#{@options[:login]}.#{sanitize_order_id(options[:order_id])}.#{amount(money)}.#{options[:currency] || currency(money)+".GBP"}.#{ credit_card.class == Hash ? credit_card[:payer_ref] : credit_card.number  }")
         end
              #raise xml.inspect
@@ -297,9 +297,9 @@ module ActiveMerchant
             xml.tag! 'xid', options[:xid]
             xml.tag! 'eci', options[:eci]
           end
-          puts shamaker(timestamp,money)
+          puts shamaker(timestamp,money, options)
 
-          xml.tag! 'sha1hash', sha1from(shamaker(timestamp,money))
+          xml.tag! 'sha1hash', sha1from(shamaker(timestamp,money, options))
           xml.tag! 'comments' do
             xml.tag! 'comment', options[:description], 'id' => 1
             xml.tag! 'comment', 'id' => 2
@@ -311,8 +311,8 @@ module ActiveMerchant
 
       end
 
-      def shamaker(timestamp,money)
-        "#{timestamp}.#{@options[:login]}.#{sanitize_order_id(options[:order_id])}.#{amount(money)}.#{options[:currency] || currency(money)+".GBP"}.#{ credit_card.class == Hash ? credit_card[:payer_ref] : credit_card.number  }"
+      def shamaker(timestamp,money, options)
+        "#{timestamp}.#{@options[:login]}.#{sanitize_order_id(options[:order_id])}.#{amount(money)}.#{options[:currency] || "#{currency(money)}.GBP"}.#{ credit_card.class == Hash ? credit_card[:payer_ref] : credit_card.number  }"
       end
 
       def parse_credit_card_number(request)
@@ -397,7 +397,7 @@ module ActiveMerchant
           end
 
         # end
-          xml.tag! 'sha1hash', sha1from(shamaker(timestamp,money))
+          xml.tag! 'sha1hash', sha1from(shamaker(timestamp,money, options))
           xml.tag! 'comments' do
             xml.tag! 'comment', options[:description], 'id' => 1
             xml.tag! 'comment', 'id' => 2
